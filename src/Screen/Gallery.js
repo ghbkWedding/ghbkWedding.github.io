5858d1aa9d657145386daabf7c65b60ee034b341
gyu gyu <gyugyu@gyuui-MacBookPro.local>
2022-01-24 21:35:27 +0900

    fix error

--- src/Screen/Gallery.js
+++ src/Screen/Gallery.js
@@ -1,21 +1,15 @@
 import React from "react";
 import ImageGallery from "react-image-gallery";
-import PhotoGallery from "react-photo-gallery";
 import { Row, Col } from "reactstrap";
 
 export default function Gallery(props) {
-  const { image, jeju } = props.config;
+  const { image } = props.config;
 
   return (
-    <div>
-      <Row>
-        <Col className="mt-5 gallery">
-          <ImageGallery items={image.gallery} />
-        </Col>
-      </Row>
-      {/* <Row className="jeju-gallery">
-        <PhotoGallery photos={jeju} />
-      </Row> */}
-    </div>
+    <Row>
+      <Col className="mt-5">
+        <ImageGallery items={image.gallery} />
+      </Col>
+    </Row>
   );
 }
